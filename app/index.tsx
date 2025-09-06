0993e0eddd8afb1a12f5ef3f1941dc069a1711f2
rohit singh <codex.rohit.singh@gmail.com>
2025-09-06 16:45:14 +0530

    .

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+ const OpenCamera = () =>{
+    router.push("/camera")
+ }
+  const SelectImage = ()=>{
+    router.push("/imagePicker")
+  }
+  const LivePhoto= ()=>{
+    router.push("/livePhoto")
+  }
+   const MapParJao= ()=>{
+    router.push("/Map")
+  }
+ 
+  return (
+
+    <View>
+
+<TouchableOpacity onPress={OpenCamera}>
+  <Text>Open camera </Text>
+</TouchableOpacity>
+<TouchableOpacity onPress={SelectImage}>
+    <Text>Select Image </Text>
+</TouchableOpacity>
+<TouchableOpacity onPress={LivePhoto}>
+  <Text>Click photo</Text>
+</TouchableOpacity>
+
+<TouchableOpacity onPress={MapParJao}>
+  <Text>Open map</Text>
+</TouchableOpacity>
+    </View>
+  );
+}
