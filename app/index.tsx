bfa246bbdef738c3e5e256a42aaa72a468b36efe
rohit singh <codex.rohit.singh@gmail.com>
2025-09-08 13:39:16 +0530

    >

--- app/index.tsx
+++ app/index.tsx
@@ -24,6 +24,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <TravelLoginScreen/>
+
       {/* <OTP/> */}
       
       {/* Navigation buttons - uncomment when needed */}
